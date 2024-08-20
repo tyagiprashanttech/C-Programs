dac242d74d7557fb3d2ae254ce1c6e7390118762
tyagiprashanttech <133961058+tyagiprashanttech@users.noreply.github.com>
2024-08-20 22:33:17 +0530

    Simple Calculator

---
+++ SimpleCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class SimpleCalculator
+    {
+        static void Main()
+        {
+            float a, b;
+            Console.WriteLine("Enter two no's =");
+            a=Convert.ToSingle(Console.ReadLine());
+            b=Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Addition is = " + (a + b));
+            Console.WriteLine("Subraction is = " + (a - b));
+            Console.WriteLine("Multiplication is = " + (a * b));
+            Console.WriteLine("Division is = " + (a / b));
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
